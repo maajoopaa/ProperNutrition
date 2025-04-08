2985453672c16d4676d3ea2da7913961965dd32a
Opa Maj <saintkilla79@gmail.com>
2025-04-08 16:40:36 +0300

    Data access entities

---
+++ ProperNutrition.DataAccess/Entities/DishEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProperNutrition.DataAccess.Entities
+{
+    public class DishEntity
+    {
+        public Guid Id { get; set; }    
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public byte[] Image { get; set; } = null!;
+
+        public virtual ICollection<UserEntity> LikedBy { get; set; } = null!;
+
+        public Guid CreatedById { get; set; }
+
+        public virtual UserEntity CreatedBy { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public virtual ICollection<DishProductEntity> Products { get; set; } = [];
+    }
+}
