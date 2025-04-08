59f7877e9a7398ff4c1b18e8d71a86471c923f05
Opa Maj <saintkilla79@gmail.com>
2025-04-08 16:40:52 +0300

    Data access mappers

---
+++ ProperNutrition.DataAccess/Mappers/DishProductMapper.cs
@@ -0,0 +1,28 @@
+﻿using ProperNutrition.DataAccess.Entities;
+using ProperNutrition.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProperNutrition.DataAccess.Mappers
+{
+    public static class DishProductMapper
+    {
+        public static DishProduct ToDomain(DishProductEntity entity) => new DishProduct
+        {
+            Product = ProductMapper.ToDomain(entity.Product),
+            Dish = DishMapper.ToDomain(entity.Dish),
+            Weight = entity.Weight
+        };
+
+        public static DishProductEntity ToEntity(DishProduct domain) => new DishProductEntity
+        {
+            DishId = domain.Dish.Id,
+            ProductId = domain.Product.Id,
+            Weight = domain.Weight
+        };
+    }
+
+}
