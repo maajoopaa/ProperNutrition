ae7899e54b9b67e51f058e742bf647b351c51530
Opa Maj <saintkilla79@gmail.com>
2025-04-12 16:47:30 +0300

    Migrations

---
+++ ProperNutrition.API/Migrations/20250411124722_InitialCreate.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProperNutrition.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Head = table.Column<string>(type: "character varying(23)", maxLength: 23, nullable: false),
+                    Body = table.Column<string>(type: "character varying(23)", maxLength: 23, nullable: false),
+                    Image = table.Column<byte[]>(type: "bytea", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(23)", maxLength: 23, nullable: false),
+                    Description = table.Column<string>(type: "character varying(23)", maxLength: 23, nullable: false),
+                    Image = table.Column<byte[]>(type: "bytea", nullable: false),
+                    Calories = table.Column<double>(type: "double precision", nullable: false),
+                    Proteins = table.Column<double>(type: "double precision", nullable: false),
+                    Fats = table.Column<double>(type: "double precision", nullable: false),
+                    Carbs = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    IsAdmin = table.Column<byte>(type: "smallint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dishes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(23)", maxLength: 23, nullable: false),
+                    Description = table.Column<string>(type: "character varying(23)", maxLength: 23, nullable: false),
+                    Image = table.Column<byte[]>(type: "bytea", nullable: false),
+                    CreatedById = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dishes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dishes_Users_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DishEntityUserEntity",
+                columns: table => new
+                {
+                    FavouriteId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LikedById = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DishEntityUserEntity", x => new { x.FavouriteId, x.LikedById });
+                    table.ForeignKey(
+                        name: "FK_DishEntityUserEntity_Dishes_FavouriteId",
+                        column: x => x.FavouriteId,
+                        principalTable: "Dishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DishEntityUserEntity_Users_LikedById",
+                        column: x => x.LikedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DishProducts",
+                columns: table => new
+                {
+                    DishId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Weight = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DishProducts", x => new { x.ProductId, x.DishId });
+                    table.ForeignKey(
+                        name: "FK_DishProducts_Dishes_DishId",
+                        column: x => x.DishId,
+                        principalTable: "Dishes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DishProducts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DishEntityUserEntity_LikedById",
+                table: "DishEntityUserEntity",
+                column: "LikedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dishes_CreatedById",
+                table: "Dishes",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DishProducts_DishId",
+                table: "DishProducts",
+                column: "DishId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+
+            migrationBuilder.DropTable(
+                name: "DishEntityUserEntity");
+
+            migrationBuilder.DropTable(
+                name: "DishProducts");
+
+            migrationBuilder.DropTable(
+                name: "Dishes");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ ProperNutrition.API/Migrations/20250412134524_UpdateConfigurations.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProperNutrition.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateConfigurations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Products",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(23)",
+                oldMaxLength: 23);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(23)",
+                oldMaxLength: 23);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Dishes",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(23)",
+                oldMaxLength: 23);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Dishes",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(23)",
+                oldMaxLength: 23);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Head",
+                table: "Articles",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(23)",
+                oldMaxLength: 23);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Articles",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(23)",
+                oldMaxLength: 23);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Products",
+                type: "character varying(23)",
+                maxLength: 23,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "character varying(23)",
+                maxLength: 23,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Dishes",
+                type: "character varying(23)",
+                maxLength: 23,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Dishes",
+                type: "character varying(23)",
+                maxLength: 23,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Head",
+                table: "Articles",
+                type: "character varying(23)",
+                maxLength: 23,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Body",
+                table: "Articles",
+                type: "character varying(23)",
+                maxLength: 23,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150);
+        }
+    }
+}
