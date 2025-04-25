ea170b56cf962c872a096fe12462da71ec47d486
Opa Maj <saintkilla79@gmail.com>
2025-04-25 08:46:08 +0300

    Models

---
+++ ProperNutrition.Application/Models/UserRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProperNutrition.Application.Models
+{
+    public class UserRequest
+    {
+        public string? Username { get; set; } 
+
+        public string? Email { get; set; }
+
+        public string CurrentPassword { get; set; } = null!;
+
+        public string? Password { get; set; }
+    }
+}
